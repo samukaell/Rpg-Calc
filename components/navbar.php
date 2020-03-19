84fa530f506a9ab7e546693b53795c9223b04a01
luanerdy <luan.vini1@gmail.com>
2020-03-18 21:32:53 -0300

    Logotipo adicionado

--- components/navbar.php
+++ components/navbar.php
@@ -1,5 +1,5 @@
 <nav>
-	<div id="logo"><img src="#"></div>
+	<div id="logo"><img src="/rpg-calc/img/logo.svg" alt="logotipo do site RPG Calc"></div>
 	<ul>
 		<li><a href="/rpg-calc">Início</a></li>
 		<li><a href="/rpg-calc/pages/criador.php">Criar ficha</a></li>
