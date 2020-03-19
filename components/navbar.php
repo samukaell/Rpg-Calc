b8e9bfd4ff0e722e5d2e3d79abf0c9b802146078
luanerdy <luan.vini1@gmail.com>
2020-03-19 20:55:07 -0300

    menu responsivo adicionado

--- components/navbar.php
+++ components/navbar.php
@@ -1,9 +1,11 @@
 <nav>
 	<div id="logo"><img src="/rpg-calc/img/logo.svg" alt="logotipo do site RPG Calc"></div>
+	<div id="botao"><img src="/rpg-calc/img/menu.svg"></div>
 	<ul>
 		<li><a href="/rpg-calc">Início</a></li>
 		<li><a href="/rpg-calc/pages/criador.php">Criar ficha</a></li>
 		<li><a href="#">Lista de fichas</a></li>
 		<li><a href="#">Login</a></li>
-	</ul>	
+	</ul>
+
 </nav>
---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+	var botao = document.querySelector('#botao');
+	var nav = document.querySelector('nav');
+	botao.addEventListener('click', (event) => {
+		if (nav.classList[0] == 'active') {
+			nav.setAttribute('class', '');
+		}
+		else{
+			nav.setAttribute('class', 'active');
+		}
+	})
+}
